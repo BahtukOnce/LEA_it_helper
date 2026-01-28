923a7c870213b20b5b89f3bc30dd511b1f18e409
Лязин Егор Александрович <ea.liazin@skolca.ru>
2026-01-28 10:46:01 +0300

    update 2026-01-28 10:46

--- push.ps1
+++ push.ps1
@@ -1,9 +1,14 @@
 git add .
 
-$msg = Read-Host "Commit message"
+$msg = Read-Host "Commit message (Enter = auto)"
+
+if ([string]::IsNullOrWhiteSpace($msg)) {
+    $msg = "update " + (Get-Date -Format "yyyy-MM-dd HH:mm")
+}
 
 git commit -m "$msg"
-
 git push
 
-Write-Output "Done! Pushed to GitHub."
+Get-Content "bot.py" | Set-Content "C:\Users\ea.liazin\Desktop\bot.txt"
+
+Write-Output "Done!"
